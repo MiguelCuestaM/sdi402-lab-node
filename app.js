7f79b788ba0e33c91d9a84980869f9c7f8716ea0
Miguel Cuesta Martínez <UO258220@uniovi.es>
2020-03-25 11:24:36 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
 app.use(express.static('public'));
 
 //Variable
@@ -15,9 +18,9 @@ app.set('port', 8081);
 app.set('db', "mongodb://admin:sdi@tiendamusica-shard-00-00-z7wpp.mongodb.net:27017,tiendamusica-shard-00-01-z7wpp.mongodb.net:27017,tiendamusica-shard-00-02-z7wpp.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority");
 
 //Rutas/controladores
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig, mongo);
-require("./routes/rautores.js")(app, swig);
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
+require("./routes/rautores.js")(app, swig, gestorBD);
 
 // lanzar el servidor
 app.listen(app.get('port'), function () {
